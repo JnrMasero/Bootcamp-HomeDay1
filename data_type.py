4c74ad6310a752fcbf1fdf4298d595184f70b118
Collins Masero <jnrmasero@gmail.com>
2016-10-31 05:37:55 -0700

    Update data_type.py

--- data_type.py
+++ data_type.py
@@ -7,22 +7,27 @@
 # For integers return a string showing how it compares to hundred e.g. For 67 return 'less than 100' for 4034 return 'more than 100' or equal to 100 as the case may be
 # For lists return the 3rd item, or None if it doesn't exist
 
-def data_type(data):
-    if data == None:
-        return 'no value'
-    elif type(data) == str:
-        return len(data)
-    elif type(data) == int:
-        if data < 100 :
-            return 'less than 100'
-        else :
-            return 'more than 100'
+def data_type(a):
+	"""Takes one input and returns a value in respect to data type of the input  """
+	a_type =type(a)
+	if a_type == str:
+		return len(a)
+	elif a_type ==bool:
+		return a
+	elif a_type ==int:
+		if a == 100:
+			return 'equal to 100'
+		elif a < 100:
+			return 'less than 100'
 
-    elif type(data) == bool:
-        return data
+		else:
+			return 'more than 100'
 
-    elif type(data) == list:
-        if len (data) >=3 :
-            return len (data)
-        else:
-            return None
+	elif a_type ==list:
+		try:
+			if a[2]:
+				return a[2]
+		except(IndexError):
+			return None
+	else:
+		return 'no value'
